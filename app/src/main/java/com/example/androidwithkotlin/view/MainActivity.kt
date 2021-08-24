5025f69041e947e35c338d95d77b13cca2f7a097
jacso <dudin10@mail.ru>
2021-08-24 17:13:21 +0300

    viewBinding для activity

--- app/src/main/java/com/example/androidwithkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/androidwithkotlin/view/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.androidwithkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.androidwithkotlin.R
+import com.example.androidwithkotlin.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
     }
