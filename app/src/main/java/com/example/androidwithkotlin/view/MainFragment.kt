1cd01b8da005d5b8d97b3e7d4733feb6e1e23916
jacso <dudin10@mail.ru>
2021-08-24 16:47:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidwithkotlin/view/MainFragment.kt
@@ -0,0 +1,44 @@
+package com.example.androidwithkotlin.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.androidwithkotlin.R
+import com.example.androidwithkotlin.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<Any> { renderData(it) }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderData(data: Any?) {
+        Toast.makeText(context, "data", Toast.LENGTH_SHORT).show()
+    }
+
+}
