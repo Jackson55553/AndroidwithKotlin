73c6ca9698fce7d9b03615152fc490272f99d3cc
Evgeny <dudin10@mail.ru>
2021-08-31 13:56:48 +0300

    4 урок

--- app/src/main/java/com/example/androidwithkotlin/view/MainFragment.kt
+++ app/src/main/java/com/example/androidwithkotlin/view/MainFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidwithkotlin.R
-import com.example.androidwithkotlin.data.Weather
 import com.example.androidwithkotlin.databinding.MainFragmentBinding
 import com.example.androidwithkotlin.ui.model.AppState
 import com.example.androidwithkotlin.viewmodel.MainViewModel
@@ -20,7 +19,7 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     private var _binding: MainFragmentBinding? = null
     private val binding
         get() = _binding!!
@@ -35,34 +34,22 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-        adapter.remove()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        adapter.setOnItemViewClickListener(object : onItemViewClickListener {
-            override fun onItemViewClick(weather: Weather) {
-                val manager = activity?.supportFragmentManager
-                if (manager != null) {
-                    val bundle = Bundle()
-                    bundle.putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
-                    manager.beginTransaction()
-                        .add(R.id.container, DetailsFragment.newInstance(bundle))
-                        .addToBackStack("")
-                        .commitAllowingStateLoss()
-                }
+        adapter.setOnItemViewClickListener { weather ->
+            activity?.supportFragmentManager?.let { manager ->
+                val bundle = Bundle()
+                bundle.putParcelable(DetailsFragment.BUNDLE_EXTRA, weather)
+                manager.beginTransaction()
+                    .add(R.id.container, DetailsFragment.newInstance(bundle))
+                    .addToBackStack("")
+                    .commitAllowingStateLoss()
             }
-        })
-        binding.mainFragmentRecyclerView.adapter = adapter
-        binding.mainFragmentFAB.setOnClickListener {
-            changeWeatherDataSet()
+        }
+        with(binding) {
+            mainFragmentRecyclerView.adapter = adapter
+            mainFragmentFAB.setOnClickListener {
+                changeWeatherDataSet()
+            }
         }
         val observer = Observer<AppState> { a ->
             renderData(a)
@@ -104,10 +91,6 @@ class MainFragment : Fragment() {
             }
 
         }
-    }
-
-    interface onItemViewClickListener {
-        fun onItemViewClick(weather: Weather)
     }
 }
 
